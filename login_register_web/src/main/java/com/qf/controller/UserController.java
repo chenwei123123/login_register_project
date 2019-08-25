4e51fec18d645b475501e1aae0dfa825ca6bf4bf
chenwei <843405214@qq.com>
2019-08-25 23:44:53 +0800

    第四次

--- login_register_web/src/main/java/com/qf/controller/UserController.java
+++ login_register_web/src/main/java/com/qf/controller/UserController.java
@@ -113,7 +113,7 @@ public class UserController {
         map.put("password",password);
         List<User> users = userService.getUserByName(map);
         if(users!=null){
-            return  "success";
+            return  "success1";
         }
         return  "login";
     }
